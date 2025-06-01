59fc33ce8de61f195ee512f66312c77995efa59e
NaritaShosei <onigirikun7171@gmail.com>
2025-06-02 03:23:37 +0900

    オブジェクトを動かす

---
+++ Assets/Script/Move.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    [SerializeField] Rigidbody _rigidBody;
+    bool isStop = false;
+    [SerializeField] Transform _goal;
+    [SerializeField] float _moveSpeed;
+    void Awake()
+    {
+        _rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isStop)
+        {
+            //確実に停止させる
+            _rigidBody.linearVelocity = Vector3.zero;
+            return;
+        }
+
+        var dir = _goal.position - transform.position;
+
+        if (dir.magnitude < 0.3f)
+        {
+            isStop = true;
+        }
+
+        _rigidBody.linearVelocity = dir.normalized * _moveSpeed;
+    }
+}
