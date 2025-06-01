52f598e615f8f6145c317ef38d1062b9efcd82b3
NaritaShosei <onigirikun7171@gmail.com>
2025-06-02 03:47:15 +0900

    カメラ

---
+++ Assets/Script/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    [SerializeField] Transform _followTarget;
+    [SerializeField] Vector3 _offset;
+    void Start()
+    {
+        transform.position = _followTarget.position + _offset;
+    }
+    private void LateUpdate()
+    {
+        transform.position = _followTarget.position + _offset;
+    }
+}
